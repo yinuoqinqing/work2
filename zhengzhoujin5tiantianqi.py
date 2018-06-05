44416606c8092385e4b95c7f2cd9e9195d13baa5
yinuoqinqing <39868179+yinuoqinqing@users.noreply.github.com>
2018-06-05 08:23:21 +0800

    Update zhengzhoujin5tiantianqi.py
    代码修改

--- zhengzhoujin5tiantianqi.py
+++ zhengzhoujin5tiantianqi.py
@@ -17,7 +17,7 @@ data=json.loads(info)
 city=data['city']['name']
 print("当前城市："+str(city))
 
-for i in range (38):
+for i in range (len(data['list'])):
     day=data['list'][i]['dt_txt']
     print("日期和时刻："+str(day))
     description=data['list'][i]['weather'][0]['description']
@@ -31,4 +31,25 @@ for i in range (38):
         print("***********************************************************")
     else:
         print("--------------------------------------------------")
+        
+        
+        
+ #函数
+def weather(data,i):
+    for i in range (len(data['list'])):
+        day=data['list'][i]['dt_txt']
+        print("日期和时刻："+str(day))
+        description=data['list'][i]['weather'][0]['description']
+        print("当前天气状况:"+str(description))
+        temp=data['list'][i]['main']['temp']
+        print("当前天气温度："+str(temp)+"开摄氏度")
+        pressure=data['list'][i]['main']['pressure']
+        print("当前气压为："+str(pressure))
+        if str(data['list'][i]['dt_txt'])[9:10]!=str(data['list'][i+1]['dt_txt'])[9:10]:
+            print("***********************************************************")
+        else:
+            print("--------------------------------------------------")
+            
+for i in range(38):
+    weather(data,i)
     
